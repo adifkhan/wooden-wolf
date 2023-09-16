8934d7f3bfc48f6a00ac6f09ee075085ffa55f93
adifkhan <pkadif@gmail..com>
2023-09-16 14:46:13 +0600

    features updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,11 +6,11 @@ const About = () => {
     <div className={style.about_container}>
       <h1>About WoodenWolf</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum repellat
-        excepturi dolorem repudiandae error dicta animi perferendis quos dolor,
-        debitis dignissimos corrupti corporis, sit ipsam odio officiis suscipit
-        cum quidem sequi! Eius, totam aut aliquid nulla earum ad fuga sint error
-        in laboriosam cupiditate.
+        Eligendi mollitia animi enim tempore pariatur placeat quaerat! Dolor,
+        similique doloribus! Veritatis corrupti incidunt dicta corporis
+        blanditiis provident, illum, quasi quis ratione mollitia ipsam cumque
+        qui error libero molestiae tempore doloremque deleniti vitae maiores, ut
+        culpa voluptates?
       </p>
     </div>
   );
